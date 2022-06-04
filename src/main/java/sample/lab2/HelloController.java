02eea56f2a95f321748405503ebf9b3e06caf6af
JDrewniak2 <saylen223@wp.pl>
2022-06-04 11:28:27 +0200

    Dodanie akcji kliknięcia

--- src/main/java/sample/lab2/HelloController.java
+++ src/main/java/sample/lab2/HelloController.java
@@ -1,14 +1,21 @@
 package sample.lab2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    public Button bntMain;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onBntAction(ActionEvent actionEvent) {
+
     }
 }
