32bffb10dbce276a7c6dac11042b9a248957f22b
JDrewniak2 <saylen223@wp.pl>
2022-06-04 17:26:06 +0200

    Zadanie 6 obliczenia

--- src/main/java/sample/lab2/HelloController.java
+++ src/main/java/sample/lab2/HelloController.java
@@ -24,16 +24,34 @@ public class HelloController {
 
     public void onBntAction(ActionEvent actionEvent) {
 
-        String promienw =promien.getText();
-        String wysokoscw =wysokosc.getText();
-        String gruboscw =grubosc.getText();
+        String promienw = promien.getText();
+        String wysokoscw = wysokosc.getText();
+        String gruboscw = grubosc.getText();
 
+        double calanierdzewna = 0;
+        double calastalowa = 0;
+        double wagastalowa = 7.85d; // waga blachy z internetu m^2
+        double nierdzewnawaga = 8; // waga blachy z internetu m^2
         double dpromienw = Double.parseDouble(promienw);
         double dwysokoscw = Double.parseDouble(wysokoscw);
         double dgruboscw = Double.parseDouble(gruboscw);
 
-   if(dpromienw <= 0 || dwysokoscw <= 0 || dgruboscw <= 0){
-       System.out.printf("Test");
+//        if (dpromienw <= 0 || dwysokoscw <= 0 || dgruboscw <= 0) {
+//            System.out.println("Błąd");
+//            return;
+//        }
+        double poleb = 2 * 3.14 * dpromienw * dwysokoscw;
+        double polep = 2 * 3.14 * dpromienw * dpromienw;
+        double polec = poleb + polep;
+        String ilemetrow = Double.toString(polec / 10000);
+        calastalowa = (polec / 10000) * dgruboscw * wagastalowa;
+        calanierdzewna = (polec / 10000) * dgruboscw * nierdzewnawaga;
+        String ilemetrowS = Double.toString(calastalowa);
+        String ilemetrowN = Double.toString(calanierdzewna);
+
+
+        System.out.println("Do stworzenia walca stalowego potrzeba będzie " + ilemetrow + " metrów kwadratowych materiału, waży ona " + ilemetrowS + " kg");
+        System.out.println("Do stworzenia walca stalowego nierdzewnego potrzeba będzie " + ilemetrow + " metrów kwadratowych materiału, waży ona " + ilemetrowN + " kg");
+
     }
-}
 }
